ab85e0245d3916bb13c5600fd563bdc4fbb811fd
Richard Noble <nobleisthyname@gmail.com>
2015-10-18 15:12:56 -0400

    Border Checking
    Added code to Wrestler movement that attempts to stop Wrestlers from flying off
    the screen. It's still a little buggy but hopefully it won't matter once we
    incorporate stages for the Wrestlers to fall off of. Also refactored Logic code
    to reduce redundancy.

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -1,6 +1,105 @@
 #include "Logic.h"
 #include <cmath>
-#include <iostream>
+
+void moveWrestler(Wrestler& sumo, int dir, float move_factor)
+{
+    // move left
+    if (dir == 1) {
+        // make sure we stay on the screen
+        if (sumo.getX() > 0) {
+            sumo.setXSpd(sumo.getXSpd()-move_factor-0.001);
+            sumo.setX(sumo.getMovedX());
+        }
+        else {
+            sumo.setX(0);
+            sumo.setXSpd(0);
+        }
+    }
+
+    // move right
+    else if (dir == 3) {
+        // make sure we stay on the screen
+        if (sumo.getX() < (800-sumo.getWidth())) {
+            sumo.setXSpd(sumo.getXSpd()+move_factor+0.001);
+            sumo.setX(sumo.getMovedX());
+        }
+        else {
+            sumo.setX(800-sumo.getWidth());
+            sumo.setXSpd(0);
+        }
+    }
+
+    // if we don't move, slow us down until we stop (friction?)
+    else if (dir == 4) {
+        if (sumo.getXSpd() < 0) {
+            if (sumo.getX() > 0) {
+                sumo.setXSpd(sumo.getXSpd()+move_factor+0.001);
+                sumo.setX(sumo.getMovedX());
+            }
+            else {
+                sumo.setX(0);
+                sumo.setXSpd(0);
+            }
+        }
+        else {
+            if (sumo.getX() < (800-sumo.getWidth())) {
+                sumo.setXSpd(sumo.getXSpd()-move_factor-0.001);
+                sumo.setX(sumo.getMovedX());
+            }
+            else {
+                sumo.setX(800-sumo.getWidth());
+                sumo.setXSpd(0);
+            }
+        }
+    }
+
+    // move up
+    else if (dir == 0) {
+        if (sumo.getY() > 0) {
+            sumo.setYSpd(sumo.getYSpd()-move_factor-0.001);
+            sumo.setY(sumo.getMovedY());
+        }
+        else {
+            sumo.setY(0);
+            sumo.setYSpd(0);
+        }
+    }
+
+    // move down
+    else if (dir == 2) {
+        if (sumo.getY() < (600-sumo.getHeight())) {
+            sumo.setYSpd(sumo.getYSpd()+move_factor+0.001);
+            sumo.setY(sumo.getMovedY());
+        }
+        else {
+            sumo.setY(600-sumo.getHeight());
+            sumo.setYSpd(0);
+        }
+    }
+
+    else if (dir == 5) {
+        if (sumo.getYSpd() < 0) {
+            if (sumo.getY() > 0) {
+                sumo.setYSpd(sumo.getYSpd()+move_factor+0.001);
+                sumo.setY(sumo.getMovedY());
+            }
+            else {
+                sumo.setY(0);
+                sumo.setYSpd(0);
+            }
+        }
+        else {
+            if (sumo.getY() < (600-sumo.getHeight())) {
+                sumo.setYSpd(sumo.getYSpd()-move_factor-0.001);
+                sumo.setY(sumo.getMovedY());
+            }
+            else {
+                sumo.setY(600-sumo.getHeight());
+                sumo.setYSpd(0);
+            }
+        }
+    }
+}
 
 void getInputAndMove(Wrestler& sumo, float elapsed_time)
 {
@@ -11,48 +110,27 @@ void getInputAndMove(Wrestler& sumo, float elapsed_time)
     float move_factor = elapsed_time*spd;
     
     // move left
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        sumo.setXSpd(sumo.getXSpd()-move_factor-0.001);
-        sumo.setX(sumo.getMovedX());
-    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        moveWrestler(sumo, 1, move_factor);
 
     // move right
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        sumo.setXSpd(sumo.getXSpd()+move_factor+0.001);
-        sumo.setX(sumo.getMovedX());
-    }
-    else {
-        if (sumo.getXSpd() < 0) {
-            sumo.setXSpd(sumo.getXSpd()+move_factor+0.001);
-            sumo.setX(sumo.getMovedX());
-        }
-        else {
-            sumo.setXSpd(sumo.getXSpd()-move_factor-0.001);
-            sumo.setX(sumo.getMovedX());
-        }
-    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        moveWrestler(sumo, 3, move_factor);
+
+    // if we don't move, slow us down
+    else
+        moveWrestler(sumo, 4, move_factor);
 
     // move up
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        sumo.setYSpd(sumo.getYSpd()-move_factor-0.001);
-        sumo.setY(sumo.getMovedY());
-    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        moveWrestler(sumo, 0, move_factor);
 
     // move down
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        sumo.setYSpd(sumo.getYSpd()+move_factor+0.001);
-        sumo.setY(sumo.getMovedY());
-    }
-    else {
-        if (sumo.getYSpd() < 0) {
-            sumo.setYSpd(sumo.getYSpd()+move_factor+0.001);
-            sumo.setY(sumo.getMovedY());
-        }
-        else {
-            sumo.setYSpd(sumo.getYSpd()-move_factor-0.001);
-            sumo.setY(sumo.getMovedY());
-        }
-    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        moveWrestler(sumo, 2, move_factor);
+
+    else
+        moveWrestler(sumo, 5, move_factor);
 }
 
 void moveAI(Wrestler& ai_sumo, Wrestler human_sumo, float elapsed_time)
@@ -69,48 +147,25 @@ void moveAI(Wrestler& ai_sumo, Wrestler human_sumo, float elapsed_time)
     sf::Vector2f ai_loc(ai_sumo.getX(), ai_sumo.getY());
 
     // move left
-    if (player_loc.x < ai_loc.x) {
-        ai_sumo.setXSpd(ai_sumo.getXSpd()-move_factor-0.001);
-        ai_sumo.setX(ai_sumo.getMovedX());
-    }
+    if (player_loc.x < ai_loc.x)
+        moveWrestler(ai_sumo, 1, move_factor);
 
     // move right
-    else if (player_loc.x > ai_loc.x) {
-        ai_sumo.setXSpd(ai_sumo.getXSpd()+move_factor+0.001);
-        ai_sumo.setX(ai_sumo.getMovedX());
-    }
+    else if (player_loc.x > ai_loc.x)
+        moveWrestler(ai_sumo, 3, move_factor);
 
-    else {
-        if (ai_sumo.getXSpd() < 0) {
-            ai_sumo.setXSpd(ai_sumo.getXSpd()+move_factor+0.001);
-            ai_sumo.setX(ai_sumo.getMovedX());
-        }
-        else {
-            ai_sumo.setXSpd(ai_sumo.getXSpd()-move_factor-0.001);
-            ai_sumo.setX(ai_sumo.getMovedX());
-        }
-    }
+    // if we don't move, do friction stuff
+    else
+        moveWrestler(ai_sumo, 4, move_factor);
 
     // move up
-    if (player_loc.y < ai_loc.y) {
-        ai_sumo.setYSpd(ai_sumo.getYSpd()-move_factor-0.001);
-        ai_sumo.setY(ai_sumo.getMovedY());
-    }
+    if (player_loc.y < ai_loc.y)
+        moveWrestler(ai_sumo, 0, move_factor);
 
     // move down
-    else if (player_loc.y > ai_loc.y) {
-        ai_sumo.setYSpd(ai_sumo.getYSpd()+move_factor+0.001);
-        ai_sumo.setY(ai_sumo.getMovedY());
-    }
+    else if (player_loc.y > ai_loc.y)
+        moveWrestler(ai_sumo, 2, move_factor);
 
-    else {
-        if (ai_sumo.getYSpd() < 0) {
-            ai_sumo.setYSpd(ai_sumo.getYSpd()+move_factor+0.001);
-            ai_sumo.setY(ai_sumo.getMovedY());
-        }
-        else {
-            ai_sumo.setYSpd(ai_sumo.getYSpd()-move_factor-0.001);
-            ai_sumo.setY(ai_sumo.getMovedY());
-        }
-    }
+    else
+        moveWrestler(ai_sumo, 5, move_factor);
 }
