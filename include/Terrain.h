c767678386d6e585176a1e40edf6cf9a150b204b
skbramlett <skbramlett@email.wm.edu>
2015-10-20 13:55:24 -0400

    basic tilemap

---
+++ include/Terrain.h
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+
+class Terrain : public sf::Drawable, public sf::Transformable
+{
+public:
+	Terrain();
+	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+private:
+	sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+    //bool accessable = true;
+    int accessable[]; //0 and 1 indicates whether it's accessable
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+};
