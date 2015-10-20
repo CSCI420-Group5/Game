1169ab4d4da3777c64e01898ce5360458bd0b7dc
Alexander J  Krone <ajkrone@steelhead.cs.wm.edu>
2015-10-19 23:00:27 -0400

    Added basic menu object

---
+++ include/MainMenu.h
@@ -0,0 +1,36 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+#include "SFML/Graphics.hpp"
+
+class MainMenu
+{
+    public:
+        MainMenu(float width, float height);
+        virtual ~MainMenu();
+
+        void init();
+
+    	void draw(sf::RenderWindow &window);
+
+		void moveUp();
+		void moveDown();
+
+        void navigate(sf::RenderWindow &window);
+
+        int getStatus();
+
+		int selected;
+        int inMenu;
+
+		sf::Font font;
+		sf::Text text[2];
+        sf::RectangleShape highlight;
+
+    protected:
+    private:
+
+    	
+
+};
+
+#endif // MAINMENU_H
