d799d683b8d0249a8fb9dc04ed457227dd97bc5c
Author Name <ajkrone@email.wm.edu>
2015-10-19 23:00:27 -0400

    Added basic menu object

---
+++ src/MainMenu.cpp
@@ -0,0 +1,98 @@
+#include "MainMenu.h" 
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+MainMenu::MainMenu(float width, float height)
+{
+	inMenu = 0;
+
+	font.loadFromFile("arial.ttf");
+
+	text[0].setFont(font);
+	text[0].setColor(sf::Color::White);
+	text[0].setString("Play");
+	text[0].setPosition(sf::Vector2f(width / 2, height / 2 ));
+
+	text[1].setFont(font);
+	text[1].setColor(sf::Color::White);
+	text[1].setString("Exit");
+	text[1].setPosition(sf::Vector2f(width / 2, height / 1.5));
+
+	highlight.setSize(sf::Vector2f(90, 40));
+  	highlight.setFillColor(sf::Color(150, 0, 35));
+}
+
+void init()
+{
+	
+}
+
+void MainMenu::draw(sf::RenderWindow &window)
+{
+	window.clear(sf::Color::Black);
+
+	if (selected == 0)
+    {
+    	highlight.setPosition(380, 300);
+    } else {
+    	highlight.setPosition(380, 400);
+    }
+	window.draw(highlight);
+	
+	window.draw(text[0]);
+	window.draw(text[1]);
+}
+
+void MainMenu::navigate(sf::RenderWindow &window)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	    {
+	        moveUp();
+	    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	    {
+	        moveDown();
+	    }
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+	    {
+	        if (selected == 0)
+	        {
+	        	inMenu = 1;
+	        } else {
+	        	window.close();
+	        }
+	    }
+}
+
+void MainMenu::moveUp()
+{
+	if (selected == 0)
+	{
+		selected = 1;
+		
+	} else {
+		selected = 0;
+	}
+}
+
+void MainMenu::moveDown()
+{
+	if (selected == 0)
+	{
+		selected = 1;
+	} else {
+		selected = 0;
+	}
+}
+
+int MainMenu::getStatus()
+{
+	return inMenu;
+}
+
+MainMenu::~MainMenu()
+{
+
+}
